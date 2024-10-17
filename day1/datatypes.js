e29415813fb396bee0ea503da07bb5be3a517faa
Prashant Bhandari <129883978+prashantbhandary@users.noreply.github.com>
2024-10-17 13:53:51 +0000

    data1

---
+++ day1/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"
+
+let name = "prashant";
+let age = 19;
+let isLogIn= false;
+
+//number
+//bigint
+//string => ""
+// bollean => true/false 
+//null => standalone value
+//undefined => aabi value not asigned
+//symbol => unique
+//object 
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined
---
+++ day1/variables.js
@@ -0,0 +1,14 @@
+const accountId = 3430;
+let accountEmail = "prashant@gmail.com"
+var accountPassword= "12345" //
+accountCity= "pokhara"
+let accountState;
+
+
+// accountId=11;
+accountEmail= "hc@gmail.com "
+accountPassword = " 212112"
+
+// console.log(accountId);
+
+console.table([accountId , accountEmail ,accountPassword, accountCity, accountState ])
