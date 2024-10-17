aeea13d26e5743e3c31e5a6947143df8ed67fa9a
prashantbhandary <santhprashant@gmail.com>
2024-10-17 20:43:58 +0545

    add cpnversion

---
+++ day1/conversion.js
@@ -0,0 +1,12 @@
+let score= "33"
+
+console.log(typeof(score))
+
+let valueInNum= Number(score)
+
+console.log(valueInNum)
+console.log(typeof(valueInNum))
+
+// "33=> 33 "
+//"33abc" => NaN
+// true => 1 , false=> 0
