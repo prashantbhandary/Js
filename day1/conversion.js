0ac80f9e15b9d0d1e9e2482be2020e4d626229e5
prashantbhandary <santhprashant@gmail.com>
2024-10-18 09:34:07 +0545

    add

--- day1/conversion.js
+++ day1/conversion.js
@@ -7,6 +7,25 @@ let valueInNum= Number(score)
 console.log(valueInNum)
 console.log(typeof(valueInNum))
 
+
 // "33=> 33 "
 //"33abc" => NaN
 // true => 1 , false=> 0
+
+
+//***operations************
+let str1="hello"
+let str2=" prashant"
+
+let str3= str1 + str2
+
+console.log(str3)
+
+// console.log("1" + 2 +2);
+
+// console.log(1 + 2+ "2" );
+
+console.log(+true)
+
+
+
