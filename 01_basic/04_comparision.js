86ab22b97f7412fbaf2412158a6471412f207bbb
prashantbhandary <santhprashant@gmail.com>
2024-10-18 10:02:29 +0545

    done till 4

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,39 @@
+console.log("2" >1 )
+
+// console.log("02">1)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+//=== check the both datatypes and compariosn
+console.log("2" === 2);
+
+
+// data types - summery
+/*
+primitive
+7 types;
+string;
+number;
+boolearn;
+null;
+undefined;
+symbol;
+bigint;
+
+reference type( non primitive)
+Array, Objects, Funtions
+
+
+*/ 
+const hero = ["saktiman", , "batman"]
+const myobj = {
+    name:"prashant",
+    age: 19,
+    address:"pokhara"
+}
+
+const myfun = function(){
+    console.log("hello my fun")
+}
