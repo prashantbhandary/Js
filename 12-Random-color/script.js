e73dd775a017cc04f7451dec59b1294ac299f5dc
prashantbhandary <santhprashant@gmail.com>
2024-10-24 00:07:16 +0545

    color changer

---
+++ 12-Random-color/script.js
@@ -0,0 +1,31 @@
+const randomcolor = function(){
+    const hex= '0123456789ABCDEF'
+    let color='#'
+    for(i=0;i<6;i++){
+       color += hex[Math.floor(Math.random()*16)]
+       
+    }
+    return color;
+}
+
+let interval;
+const startChangingColor= function(){
+    if(!interval){
+        interval= setInterval(change,100);
+    }
+    
+    
+    function change(){
+        document.body.style.backgroundColor = randomcolor();
+    }
+}
+
+
+const stopChangingColor = function(){
+    clearInterval(interval);
+    interval= null;
+}
+
+document.querySelector("#start").addEventListener('click', startChangingColor);
+document.querySelector("#stop").addEventListener('click',stopChangingColor);
+
