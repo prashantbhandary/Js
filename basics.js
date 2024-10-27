558ec8e9a09bc9f61c85cdae2cfd1d7875c73a4c
prashantbhandary <santhprashant@gmail.com>
2024-10-27 19:34:46 +0545

    basic js read me

---
+++ basics.js
@@ -0,0 +1,74 @@
+//word vs keyword
+
+// chacha = word
+// for == keyword
+
+//var const let
+
+// variables and constants         
+
+//score=0
+//lives
+
+
+//hosting hoisting
+//hoisting - variable and funtions are hoisted which means 
+//           there declaration is moved on the top of code
+
+console.log(a)   //undefined hunxaa tyoo var a= 12 chai 2 ota ma divide hunxa...
+var a=12;
+
+//var a;  ----> decleration  it is moved on the top ... called hoisting 
+//a=12;   ----> initialization
+
+
+//undefined and not-defined..  there is huge difference..
+//undefined banee koo existance hunxa but no value 
+// not defined tyoo chadai xaina...
+
+
+//types in js
+// primitives and Reference
+// primitives=Number ,string , null 
+
+// reference= () {} []
+//yesto koi value jaslai copy garda real copy hunna,
+//  but main value ko reference pass hunxa
+//tyo ko reference value vanxa
+
+//Ani josko copy garda real copy copy hunxa tyo lai primitive vanxa
+
+//conditionals ----  if else if else
+
+//loops repeat
+
+//funtions --6 styles of making funtions
+
+//arguments -- real value kun hami dinxam funtion chalau da 
+//parameter -- variables jumna value store hunxa argument wala
+
+// function abcd(a){  //---->parameters
+// }
+// abcd(12)  //-----> arguments
+
+//array
+//push- to add----  
+// pop- to remove ---- 
+// shift-suru ma value hatauxa 
+// unsfhit- suru ma value jodxa
+
+//slice?-assignment
+
+// splice -- jun thau deki kati ota value hatau niii is splice..
+
+//ek se jadaha manxe ko kura gare arreyy
+
+//ek manxe ko bare ma sab chizzz is objects..
+
+//objects bane ko ek manxe ko sab details hold garni
+//1. blank object
+var a={}
+
+var b={
+    age:19;
+}
--- readme.md
+++ readme.md
@@ -1,7 +1,39 @@
-# Created the four diffrent projects 
+# Documentation On JS Basics
 
-## 1. Color-Changer
-## 2. BMI Calculator
-## 3. Digital Clock
-## 4. Guessing Game
+- word vs keyword
+- var const let
+--the difference
+--window object
+--browswr context api
+--stack
+--heap memory
+--execution context
+--lexial environment
+-hoisting
+-types in js
+-primitives and reference
+--how to copy refrence value
+-conditions
+-if else else if
+--truthy or falsy values
+--switch
+-loops
+--forin for of
+-funtions
+--what is first class funnctions
+--callback fncs
+-params, arguments
+-array
+-push pops shift unshift
+--how array are made behinds the science
+-- why we can make negative indexed array in js
+--pratice ques
+-objects
+-props vs mehtods 
+-updateing objects prop
+--how to delet object prop
+--pratice questions scenarios
+
+DOM
+
 
